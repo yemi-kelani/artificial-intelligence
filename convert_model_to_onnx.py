83589a16ae14437dce7c57932039e869e2dfee17
Yemi Kelani <91276489+yemi-kelani@users.noreply.github.com>
2023-07-10 11:00:25 -0500

    conver pt to onnx

--- convert_model_to_onnx.py
+++ convert_model_to_onnx.py
@@ -1,43 +1,62 @@
+import onnx
 import torch
 import argparse
-from models.ReinforcementLearning.DeepQAgent import DeepQAgent
-
+from models.ReinforcementLearning.DeepQAgent import DeepQAgentOnnxVersion
 
 def convert_pt_to_onxx(filepath: str, input_size: tuple):
+    
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
-    # BATCH_SIZE    = 64
-    # STATE_SPACE   = 9
-    # ACTION_SPACE  = 9
-    # EPSILON       = 1.0
-    # GAMMA         = 0.95
-    # HIDDEN_SIZE   = 100
-    # DROPOUT       = 0.15
-    # TRAIN_START   = 1000
-
-    # model = DeepQAgent(
-    #     device        = DEVICE,
-    #     epsilon       = EPSILON, 
-    #     gamma         = GAMMA,
-    #     state_space   = STATE_SPACE, 
-    #     action_space  = ACTION_SPACE, 
-    #     hidden_size   = HIDDEN_SIZE,
-    #     dropout       = DROPOUT,
-    #     train_start   = TRAIN_START,
-    #     batch_size    = BATCH_SIZE
-    # )
+    BATCH_SIZE    = 64
+    STATE_SPACE   = 9
+    ACTION_SPACE  = 9
+    EPSILON       = 1.0
+    GAMMA         = 0.95
+    HIDDEN_SIZE   = 100
+    DROPOUT       = 0.15
+    TRAIN_START   = 1000
     
-    model = DeepQAgent() # REPLACE WITH APPRORIATE MODEL
+    model = DeepQAgentOnnxVersion(
+        device        = DEVICE,
+        epsilon       = EPSILON, 
+        gamma         = GAMMA,
+        state_space   = STATE_SPACE, 
+        action_space  = ACTION_SPACE, 
+        hidden_size   = HIDDEN_SIZE,
+        dropout       = DROPOUT,
+        train_start   = TRAIN_START,
+        batch_size    = BATCH_SIZE
+    )
+    
+    # model = DeepQAgentOnnxVersion() # REPLACE WITH APPRORIATE MODEL
 
     model.load_state_dict(torch.load(filepath, map_location=torch.device(DEVICE)))
     model.eval()
+    
+    print("BEGIN model parameters:")
+    print(model.parameters)
+    print("END   model parameters.")
 
     dummy_input = torch.zeros(input_size)
+    onnx_model_path = f"trained_models/onnx_models/{filepath.split('/')[-1].split('.')[0]}.onnx"
     torch.onnx.export(
         model, dummy_input, 
-        f"{filepath.split('.')[0]}.onnx", 
+        onnx_model_path, 
         verbose=True
     )
+    
+    # Check the model
+    onnx_model = onnx.load(onnx_model_path)
+
+    print(f"The model is:\n{onnx_model}")
+    
+    try:
+        onnx.checker.check_model(onnx_model)
+    except onnx.checker.ValidationError as e:
+        print(f"The model is invalid: {e}")
+    else:
+        print("The model is valid!")
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
--- models/ReinforcementLearning/DeepQAgent.py
+++ models/ReinforcementLearning/DeepQAgent.py
@@ -1,3 +1,5 @@
+import random
+import numpy as np
 import torch
 import torch.nn as nn
 from collections import deque
@@ -85,3 +87,45 @@ class DeepQAgent(nn.Module):
   def save_model(self, path):
     torch.save(self.state_dict(), path)
     print(f"Model saved to {path}")
+
+# The ONNX.js version of the model should contain only the
+# bare necessities for the forward pass of full version
+class DeepQAgentOnnxVersion(nn.Module):
+  def __init__(
+      self,
+      device,
+      epsilon:float, 
+      gamma:float,
+      state_space:int, 
+      action_space:int, 
+      hidden_size:int = 100,
+      dropout:float = 0.15,
+      train_start:int = 50,
+      batch_size:int = 64,
+  ):
+    
+    super(DeepQAgentOnnxVersion, self).__init__()
+    
+    self.epsilon = epsilon
+    self.epsilon_min = 0.001 * epsilon
+    self.epsilon_decay_rate = 0.999
+    self.gamma = gamma
+    self.state_space = state_space
+    self.action_space = action_space
+    self.train_start = train_start
+    self.batch_size = batch_size
+    self.memory = deque(maxlen=2000)
+    self.device = device
+    
+    self.model = nn.Sequential(
+        nn.Linear(state_space, hidden_size),
+        nn.Dropout(dropout),
+        nn.Tanh(),
+        nn.Linear(hidden_size, hidden_size),
+        nn.Dropout(dropout),
+        nn.Tanh(),
+        nn.Linear(hidden_size, action_space)
+    )
+
+  def forward(self, state):
+    return self.model(torch.flatten(state))
